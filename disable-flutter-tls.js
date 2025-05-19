b3342743d3aaf619b890f19a918b3322b4348c55
Jeroen Beckers <dauntless@dauntless.be>
2025-05-19 18:34:05 +0200

    Add Android/windows patterns

--- disable-flutter-tls.js
+++ disable-flutter-tls.js
@@ -31,14 +31,29 @@ var config = {
                 "FF 43 01 D1 FE 67 01 A9 ?? ?? 06 94 ?? 7? 06 94 68 1A 40 F9 15 15 41 F9 B5 00 00 B4 B6 4A 40 F9",
             ],
             "arm": [
-                "2D E9 F? 4? D0 F8 00 80 81 46 D8 F8 18 00 D0 F8 ??",
+                "2D E9 F? 4? D0 F8 00 80 81 46 D8 F8 18 00 D0 F8",
             ],
             "x64": [
-                "55 41 57 41 56 41 55 41 54 53 50 49 89 f? 4? 8b ?? 4? 8b 4? 30 4c 8b ?? ?? 0? 00 00 4d 85 ?? 74 1? 4d 8b",
-                "55 41 57 41 56 41 55 41 54 53 48 83 EC 18 49 89 FF 48 8B 1F 48 8B 43 30 4C 8B A0 28 02 00 00 4D 85 E4 74"
-                ]
+                "55 41 57 41 56 41 55 41 54 53 50 49 89 F? 4? 8B ?? 4? 8B 4? 30 4C 8B ?? ?? 0? 00 00 4D 85 ?? 74 1? 4D 8B",
+                "55 41 57 41 56 41 55 41 54 53 48 83 EC 18 49 89 FF 48 8B 1F 48 8B 43 30 4C 8B A0 28 02 00 00 4D 85 E4 74",
+                "55 41 57 41 56 41 55 41 54 53 48 83 EC 18 49 89 FE 4C 8B 27 49 8B 44 24 30 48 8B 98 D0 01 00 00 48 85 DB"
+            ],
+            "x86":[
+                "55 89 E5 53 57 56 83 E4 F0 83 EC 20 E8 00 00 00 00 5B 81 C3 2B 79 66 00 8B 7D 08 8B 17 8B 42 18 8B 80 88 01"
+            ]
+
+        }
+    },
+    "windows": {
+        "modulename": "flutter_windows.dll",
+        "patterns":{
+            "x64":[
+                "41 57 41 56 41 55 41 54 56 57 53 48 83 EC 40 4? 89 CF 48 8B 05 ?? ?? ?? 00 48 31 E0 48 89 44 24 38 4? 8B 31 4? 8B",
+                "41 57 41 56 41 55 41 54 56 57 55 53 48 83 EC 38 48 89 CF 48 8B 05 20 45 C6 00 48 31 E0 48 89 44 24 30 48 8B 31 48",
+            ]
         }
     }
+
 };
 
 console.log("[+] Arch:", Process.arch)
@@ -66,7 +81,13 @@ function disableTLSValidation() {
     console.log(`[+] Attempting to find and hook ssl_verify_peer_cert (${tries}/${maxTries})`)
 
     // Get reference to module. Necessary for iOS, and usefull check for Android
-    var platformConfig = config[Java.available ? "android" : "ios"];
+    var platformConfig = {}
+    if(Process.platform in config){
+        platformConfig = config[Process.platform]
+    }else{
+        console.log(`[!] Platform not supported: ${Process.platform}`)
+    }
+
     var m = Process.findModuleByName(platformConfig["modulename"]);
 
     if (m === null) {
